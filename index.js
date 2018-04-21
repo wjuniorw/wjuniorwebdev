ab474123ea1eed4fec245ff7226c82e3f2a84a02
wjuniorw <wilson.junior90@gmail.com>
2018-04-21 01:15:19 -0300

    porta dinamica, resolvendo erro após build no server

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.prepare()
     return handle(req, res)
   })
 
-  server.listen(2000, (err) => {
+  server.listen(port, (err) => {
     if (err) throw err
     console.log('> Ready on http://localhost:'+port)
   })

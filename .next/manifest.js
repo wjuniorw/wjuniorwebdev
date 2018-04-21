59d80417c5ed5cc5ea86b2613d6d8fc4d37f269e
wjuniorw <wilson.junior90@gmail.com>
2018-04-21 01:52:22 -0300

    try to indexing site on google

--- .next/manifest.js
+++ .next/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "a7adb572485c62a13e79"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "21b3c81c23cff3fbc066"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","3":"bundles/pages/index.js"}[chunkId]||chunkId) + "-" + {"0":"06312a54a0cbe71d5c9e","1":"3763de13a4559be52569","3":"de83761818dea88fa9f7"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js"}[chunkId]||chunkId) + "-" + {"0":"07720f4cc332e69b1001","1":"3763de13a4559be52569"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {

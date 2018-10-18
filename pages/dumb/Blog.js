7b63e30ece08e50e380eab3ea4090972c9a64fd6
Wilson junior <wilson.junior90@gmail.com>
2018-10-18 23:54:34 +0000

    teste

--- pages/dumb/Blog.js
+++ pages/dumb/Blog.js
@@ -4,7 +4,7 @@ const Blog = ({})=> (
       <h3 className="android-section-title mdl-typography--display-1-color-contrast">Últimos Posts</h3>
       <div className="android-card-container mdl-grid">
         <Post img={'/static/react.png'} date={'22/04/2018'} 
-              desc={`Introdução ao desenvolviemento de site utilizando Nextjs, um framework ssr (server side rendering) com reactjs`}
+              desc={`Introdução ao desenvolvimento de site utilizando Nextjs, um framework ssr (server side rendering) com reactjs`}
               tags={`Nextjs, reactjs e express`} />
       </div>
     </section>

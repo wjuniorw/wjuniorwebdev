08db161fae206bb80a4a2101ff2bccea37f09105
wjuniorw <wilson.junior90@gmail.com>
2020-12-30 00:05:53 -0300

    export some components

--- pages/dumb/blog-post.js
+++ pages/dumb/blog-post.js
@@ -1,108 +1,204 @@
-const blogPost = ({})=> (
-    <div class="mdl-layout__container">
-        <section class="mdl-layout mdl-js-layout mdl-layout--fixed-header has-drawer is-upgraded" data-upgraded=",MaterialLayout">
-        <div class="android-header mdl-layout__header mdl-layout__header--waterfall is-casting-shadow is-compact">
-            <div class="mdl-layout__drawer-button"><i class="material-icons">menu</i></div>
-            <div class="mdl-layout__header-row">
-            <h1 class="android-font android-title mdl-layout-title mdl-typography--text-uppercase android-link">Vitor Leal</h1>
-            <div class="android-header-spacer mdl-layout-spacer"></div>
-            <div class="android-navigation-container">
-                <nav class="android-navigation mdl-navigation">
-                    <a class="mdl-navigation__link mdl-typography--text-uppercase" href="https://vitorleal.com/">Home</a>
-                    <a class="mdl-navigation__link mdl-typography--text-uppercase active" href="https://vitorleal.com/blog"> Blog </a>
-                    <a class="mdl-navigation__link mdl-typography--text-uppercase" href="https://vitorleal.com/projetos">Projetos </a>
-                    <a class="mdl-navigation__link mdl-typography--text-uppercase" href="https://vitorleal.com/portfolio"> Portfolio </a>
-                    <a class="mdl-navigation__link mdl-typography--text-uppercase" href="https://vitorleal.com/talks">Talks</a>
-                </nav>
-            </div>
+const BlogPost = ({}) => (
+  <div class="mdl-layout__container">
+    <section
+      class="mdl-layout mdl-js-layout mdl-layout--fixed-header has-drawer is-upgraded"
+      data-upgraded=",MaterialLayout"
+    >
+      <div class="android-header mdl-layout__header mdl-layout__header--waterfall is-casting-shadow is-compact">
+        <div class="mdl-layout__drawer-button">
+          <i class="material-icons">menu</i>
         </div>
-    </div>
-<div class="android-drawer mdl-layout__drawer">
-<nav class="mdl-navigation">
-<a class="mdl-navigation__link" href="https://vitorleal.com/">
-Home
-</a>
-<a class="mdl-navigation__link" href="https://vitorleal.com/blog">
-Blog
-</a>
-<a class="mdl-navigation__link" href="https://vitorleal.com/projetos">
-Projetos
-</a>
-<a class="mdl-navigation__link" href="https://vitorleal.com/portfolio">
-Portfolio
-</a>
-<a class="mdl-navigation__link" href="https://vitorleal.com/talks">
-Talks
-</a>
-<a class="mdl-navigation__link" href="https://github.com/vitorleal" target="_blank">
-GitHub
-</a>
-<a class="mdl-navigation__link" href="http://www.linkedin.com/in/vitorleal" target="_blank">
-LinkedIn
-</a>
-<a class="mdl-navigation__link" href="https://twitter.com/@vitorleal" target="_blank">
-Twitter
-</a>
-</nav>
-</div>
-<div class="android-content mdl-layout__content">
-<a name="top"></a>
-<h2 class="android-font blog-post-title mdl-typography--text-center">Introdução ao JavaScript</h2>
-<section class="android-blog-section android-post-section">
-<header>
-<p class="meta date">
-<time datetime="2014-05-25 22:50" pubdate="">
-25/5/2014
-</time>
-</p>
-</header>
-<h2>Aula de Introdução ao JavaScript - <a href="https://www.facebook.com/ceit.pr3g">Centro de Educação e Inovação Tecnológica CEIT PR3G</a></h2><p>Aula de introdução ao JavaScript para os desenvolvedores do CEIT em Cabrália</p>
-<script async="true" class="speakerdeck-embed" data-id="80d42640c6a3013196d9061a363cc5d8" src="//speakerdeck.com/assets/embed.js"></script>
-<h3>Deixe seu comentário</h3>
-<article id="disqus_thread"></article>
-</section>
-<footer class="android-footer mdl-mega-footer">
-<div class="mdl-mega-footer--top-section">
-<div class="mdl-mega-footer--left-section">
-</div>
-<div class="mdl-mega-footer--right-section">
-<a class="mdl-typography--font-light" href="#top">
-Ir para o topo <i class="material-icons">expand_less</i>
-</a>
-</div>
-</div>
-<div class="mdl-mega-footer--middle-section">
-<p class="mdl-typography--font-light">
-Copyright © 2016 - Vitor Leal - <a href="mailto:me@vitorleal.com">me@vitorleal.com</a>
-</p>
-</div>
-<div class="mdl-mega-footer--bottom-section">
-<a class="android-link mdl-typography--font-light" href="https://vitorleal.com/">
-Home
-</a>
-<a class="android-link mdl-typography--font-light" href="https://vitorleal.com/blog">
-Blog
-</a>
- <a class="android-link mdl-typography--font-light" href="https://vitorleal.com/projetos">
-Projetos
-</a>
-<a class="android-link mdl-typography--font-light" href="https://vitorleal.com/portfolio">
-Portfolio
-</a>
-<a class="android-link mdl-typography--font-light" href="https://vitorleal.com/talks">
-Talks
-</a>
-<a class="android-link mdl-typography--font-light" href="https://github.com/vitorleal" target="_blank">
-GitHub
-</a>
-<a class="android-link mdl-typography--font-light" href="http://www.linkedin.com/in/vitorleal" target="_blank">
-LinkedIn
-</a>
-<a class="android-link mdl-typography--font-light" href="https://twitter.com/@vitorleal" target="_blank">
-Twitter
-</a>
-</div>
-</footer>
-</div>
-<div class="mdl-layout__obfuscator"></div></section></div>
- )
+        <div class="mdl-layout__header-row">
+          <h1 class="android-font android-title mdl-layout-title mdl-typography--text-uppercase android-link">
+            Vitor Leal
+          </h1>
+          <div class="android-header-spacer mdl-layout-spacer"></div>
+          <div class="android-navigation-container">
+            <nav class="android-navigation mdl-navigation">
+              <a
+                class="mdl-navigation__link mdl-typography--text-uppercase"
+                href="https://vitorleal.com/"
+              >
+                Home
+              </a>
+              <a
+                class="mdl-navigation__link mdl-typography--text-uppercase active"
+                href="https://vitorleal.com/blog"
+              >
+                {" "}
+                Blog{" "}
+              </a>
+              <a
+                class="mdl-navigation__link mdl-typography--text-uppercase"
+                href="https://vitorleal.com/projetos"
+              >
+                Projetos{" "}
+              </a>
+              <a
+                class="mdl-navigation__link mdl-typography--text-uppercase"
+                href="https://vitorleal.com/portfolio"
+              >
+                {" "}
+                Portfolio{" "}
+              </a>
+              <a
+                class="mdl-navigation__link mdl-typography--text-uppercase"
+                href="https://vitorleal.com/talks"
+              >
+                Talks
+              </a>
+            </nav>
+          </div>
+        </div>
+      </div>
+      <div class="android-drawer mdl-layout__drawer">
+        <nav class="mdl-navigation">
+          <a class="mdl-navigation__link" href="https://vitorleal.com/">
+            Home
+          </a>
+          <a class="mdl-navigation__link" href="https://vitorleal.com/blog">
+            Blog
+          </a>
+          <a class="mdl-navigation__link" href="https://vitorleal.com/projetos">
+            Projetos
+          </a>
+          <a
+            class="mdl-navigation__link"
+            href="https://vitorleal.com/portfolio"
+          >
+            Portfolio
+          </a>
+          <a class="mdl-navigation__link" href="https://vitorleal.com/talks">
+            Talks
+          </a>
+          <a
+            class="mdl-navigation__link"
+            href="https://github.com/vitorleal"
+            target="_blank"
+          >
+            GitHub
+          </a>
+          <a
+            class="mdl-navigation__link"
+            href="http://www.linkedin.com/in/vitorleal"
+            target="_blank"
+          >
+            LinkedIn
+          </a>
+          <a
+            class="mdl-navigation__link"
+            href="https://twitter.com/@vitorleal"
+            target="_blank"
+          >
+            Twitter
+          </a>
+        </nav>
+      </div>
+      <div class="android-content mdl-layout__content">
+        <a name="top"></a>
+        <h2 class="android-font blog-post-title mdl-typography--text-center">
+          Introdução ao JavaScript
+        </h2>
+        <section class="android-blog-section android-post-section">
+          <header>
+            <p class="meta date">
+              <time datetime="2014-05-25 22:50" pubdate="">
+                25/5/2014
+              </time>
+            </p>
+          </header>
+          <h2>
+            Aula de Introdução ao JavaScript -{" "}
+            <a href="https://www.facebook.com/ceit.pr3g">
+              Centro de Educação e Inovação Tecnológica CEIT PR3G
+            </a>
+          </h2>
+          <p>
+            Aula de introdução ao JavaScript para os desenvolvedores do CEIT em
+            Cabrália
+          </p>
+          <script
+            async="true"
+            class="speakerdeck-embed"
+            data-id="80d42640c6a3013196d9061a363cc5d8"
+            src="//speakerdeck.com/assets/embed.js"
+          ></script>
+          <h3>Deixe seu comentário</h3>
+          <article id="disqus_thread"></article>
+        </section>
+        <footer class="android-footer mdl-mega-footer">
+          <div class="mdl-mega-footer--top-section">
+            <div class="mdl-mega-footer--left-section"></div>
+            <div class="mdl-mega-footer--right-section">
+              <a class="mdl-typography--font-light" href="#top">
+                Ir para o topo <i class="material-icons">expand_less</i>
+              </a>
+            </div>
+          </div>
+          <div class="mdl-mega-footer--middle-section">
+            <p class="mdl-typography--font-light">
+              Copyright © 2016 - Vitor Leal -{" "}
+              <a href="mailto:me@vitorleal.com">me@vitorleal.com</a>
+            </p>
+          </div>
+          <div class="mdl-mega-footer--bottom-section">
+            <a
+              class="android-link mdl-typography--font-light"
+              href="https://vitorleal.com/"
+            >
+              Home
+            </a>
+            <a
+              class="android-link mdl-typography--font-light"
+              href="https://vitorleal.com/blog"
+            >
+              Blog
+            </a>
+            <a
+              class="android-link mdl-typography--font-light"
+              href="https://vitorleal.com/projetos"
+            >
+              Projetos
+            </a>
+            <a
+              class="android-link mdl-typography--font-light"
+              href="https://vitorleal.com/portfolio"
+            >
+              Portfolio
+            </a>
+            <a
+              class="android-link mdl-typography--font-light"
+              href="https://vitorleal.com/talks"
+            >
+              Talks
+            </a>
+            <a
+              class="android-link mdl-typography--font-light"
+              href="https://github.com/vitorleal"
+              target="_blank"
+            >
+              GitHub
+            </a>
+            <a
+              class="android-link mdl-typography--font-light"
+              href="http://www.linkedin.com/in/vitorleal"
+              target="_blank"
+            >
+              LinkedIn
+            </a>
+            <a
+              class="android-link mdl-typography--font-light"
+              href="https://twitter.com/@vitorleal"
+              target="_blank"
+            >
+              Twitter
+            </a>
+          </div>
+        </footer>
+      </div>
+      <div class="mdl-layout__obfuscator"></div>
+    </section>
+  </div>
+);
+
+export default BlogPost
--- pages/portifolio.js
+++ pages/portifolio.js
@@ -1,1 +1,9 @@
- 
+import React from 'react'
+
+const Portfolio = () => (
+  <>
+    <h1>Portfolio</h1>
+  </>
+)
+
+export default Portfolio

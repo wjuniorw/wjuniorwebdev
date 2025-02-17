b26cea07a382aaa3419ad866440b16259eff36b9
Wilson Junior <wilson.junior90@gmail.com>
2025-02-17 00:46:38 -0300

    build(favicons): adding favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             `,
           }}
         />
+        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        <link rel='manifest' href='/site.webmanifest'></link>
       </head>
       <body>
         <ThemeProvider
